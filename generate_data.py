9799dcc3a442df767fc56b617fcccecc0209ef75
enesarda <enesarda22@gmail.com>
2025-03-08 23:46:53 -0500

    use fineweb-edu to generate data

--- generate_data.py
+++ generate_data.py
@@ -6,26 +6,20 @@ from transformers import AutoTokenizer
 
 if __name__ == "__main__":
     NUM_SAMPLES = 100_000
-    MAX_LENGTH = 1024
 
     TOKENIZER_PATH = "gpt2"
     OUTPUT_DIR = "data"
+    ds_path = "HuggingFaceFW/fineweb-edu"
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     random.seed(42)
-    redpajama_ds = load_dataset(
-        "togethercomputer/RedPajama-Data-1T",
-        "default",
-        split="train",
-        streaming=True,
-        trust_remote_code=True,
-    )
+    ds = load_dataset(ds_path, name="sample-10BT", split="train", streaming=True)
 
     samples = []
     count = 0
 
-    for example in tqdm(redpajama_ds):
-        samples.append(example)
+    for example in tqdm(ds):
+        samples.append({"text": example["text"]})
         count += 1
         if count >= NUM_SAMPLES:
             break
@@ -36,21 +30,17 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH)
 
 
-    def tokenize_fn(example):
-        return tokenizer(
-            example["text"],
-            truncation=True,
-            max_length=MAX_LENGTH
-        )
+    def tokenize_fn(samples):
+        tokens = tokenizer(samples["text"])
+
+        tokens["input_ids"] = [[tokenizer.eos_token_id] + input_ids + [tokenizer.eos_token_id] for input_ids in tokens["input_ids"]]
+        tokens["attention_mask"] = [[1] + attention_mask + [1] for attention_mask in tokens["attention_mask"]]
+        return tokens
 
 
-    tokenized_dataset = raw_dataset.map(
-        tokenize_fn,
-        batched=True,
-        remove_columns=["text"]  # drop original text column
-    )
+    tokenized_dataset = raw_dataset.map(tokenize_fn, batched=True, remove_columns=["text"])
     print("Tokenization complete.")
     print(tokenized_dataset)
 
-    tokenized_dataset.save_to_disk(os.path.join(OUTPUT_DIR, "tokenized_ds"))
+    tokenized_dataset.save_to_disk(os.path.join(OUTPUT_DIR, f"{TOKENIZER_PATH}_tokenized_fineweb-edu"))
     print(f"Saved tokenized under: {OUTPUT_DIR}")
