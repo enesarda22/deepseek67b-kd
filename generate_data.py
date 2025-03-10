35d8087c2e8b6e1692509f7fdae87c3e64ccb0f5
ubuntu <enesarda22@gmail.com>
2025-03-10 00:01:40 +0000

    change text generation code

--- generate_data.py
+++ generate_data.py
@@ -1,32 +1,66 @@
 import itertools
 import os
+from tqdm import tqdm
 
+import torch
 import numpy as np
 from datasets import load_dataset
-from transformers import AutoTokenizer, set_seed, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed
+
+
+def generate_text(batch):
+    inputs = teacher_tokenizer(batch, return_tensors="pt", padding=True, truncation=True).to("cuda")
+
+    # Generate text
+    outputs = model.generate(
+        **inputs,
+        max_new_tokens=MAX_NEW_TOKENS,
+        do_sample=True,
+        temperature=1.1,
+        top_p=0.9,
+        pad_token_id=teacher_tokenizer.eos_token_id
+    )
+
+    generated_texts = teacher_tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    return generated_texts
 
 
 if __name__ == "__main__":
     NUM_TOKENS = 1_000_000
-    MAX_NEW_TOKENS = 1024
-    BATCH_SIZE = 32
+    MAX_NEW_TOKENS = 32
+    BATCH_SIZE = 1
 
-    TEACHER_MODEL = "gpt2"
+    TEACHER_MODEL = "deepseek-ai/deepseek-llm-67b-base"
     STUDENT_MODEL = "TinyLlama/TinyLlama_v1.1"
     DS = "HuggingFaceFW/fineweb-edu"
     OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "data")
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     set_seed(42)
+
     ds = load_dataset(DS, name="sample-10BT", split="train", streaming=True)
-    generator = pipeline("text-generation", model=TEACHER_MODEL)
+    print("Dataset loaded!")
+
+    teacher_tokenizer = AutoTokenizer.from_pretrained(TEACHER_MODEL)
+    model = AutoModelForCausalLM.from_pretrained(
+        TEACHER_MODEL,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+        offload_folder="offload",
+        offload_state_dict=True,
+    )
+    print(f"Teacher model loaded to {model.hf_device_map}")
+
+    model = torch.compile(model)
+    model.eval()
+    print("Teacher model compiled!")
+
     student_tokenizer = AutoTokenizer.from_pretrained(STUDENT_MODEL)
 
     input_ids_np = np.empty(NUM_TOKENS, dtype=np.uint32)
     count = 0
-    for batch in ds.iter(batch_size=BATCH_SIZE):
-        generated_texts = generator(batch["text"], max_new_tokens=MAX_NEW_TOKENS, temperature=1.1, top_p=0.9, do_sample=True)
-        generated_texts = [b[0]["generated_text"] for b in generated_texts]
+    for batch in tqdm(ds.iter(batch_size=BATCH_SIZE)):
+        generated_texts = generate_text(batch["text"])
 
         tokens = student_tokenizer(generated_texts)
         input_ids = list(itertools.chain.from_iterable(tokens["input_ids"]))
@@ -37,6 +71,7 @@ if __name__ == "__main__":
         else:
             input_ids_np[count:count+len(input_ids)] = input_ids
             count += len(input_ids)
+        print(f"Token Count: {count}")
 
     fn = os.path.join(OUTPUT_DIR, "tokenized_fineweb-edu")
     np.save(fn, input_ids_np)
